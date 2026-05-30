ab18eb317e962d4dea48761d496a509f49ce738f
Manthan Patel <manthan002408@gmail.com>
2026-05-30 11:34:28 +0530

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL:
-        "http://localhost:5000/api"
+        "https://v-pms-backend.onrender.com/api"
 });
 
 API.interceptors.request.use(
